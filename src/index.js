a0e7976cbd014ccb0c92881b5612d34e01597375
CherokeeX <serefb80@gmail.com>
2023-03-29 14:29:29 +0300

    redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
-
+import { createStore } from 'redux';
 import reportWebVitals from './reportWebVitals';
 import AppRouter from './routers/AppRouter';
 
+
+const initialState = {count : 0}
+
+const store = createStore((state = initialState,action )=>{
+const incrementBy = typeof action.incrementBy === 'number'? action.incrementBy : 1 ;
+
+    switch (action.type){
+
+        case 'INCREMENT': 
+        return {
+            count: state.count+incrementBy
+        }
+        case 'DECREMENT':
+            return {
+                count: state.count-1
+            }
+            case 'RESET':
+            return {
+                count: 0
+            }
+        default : 
+        return state
+
+    }
+
+});
+console.log (store.getState());
+
+store.dispatch ({
+    type : 'INCREMENT',
+    incrementBy : 10
+})
+store.dispatch ({
+    type : 'INCREMENT'
+})
+
+store.dispatch ({
+    type : 'DECREMENT'
+})
+console.log (store.getState()); 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
 
     <AppRouter/>
   
