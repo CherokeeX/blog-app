e20762367a227357660ac3860a62ab2034309502
CherokeeX <serefb80@gmail.com>
2023-04-05 10:26:35 +0300

    edit links

--- src/components/BlogListItems.js
+++ src/components/BlogListItems.js
@@ -5,10 +5,11 @@ import { Link } from 'react-router-dom';
   return (
  <>
  <li key={id} >
-{title} - <Link to={`/blogs/${id}`}> Details</Link>
+{title} - <Link to={`/blogs/${id}`}> Details</Link> -<Link to={`/edit/${id}`}> Edit Blog</Link>
  </li>
  </>
   )
 }   
 
 export default BlogListItems;
+  
