155b072b0711c530360ea92e308af0ee945cbba2
CherokeeX <serefb80@gmail.com>
2023-04-05 13:46:52 +0300

    firebases

---
+++ src/firebase/fireBaseConfig.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDUF3nnsFMV73_hN-j3W9ZC72XGDnef5gc",
+    authDomain: "react-blog-app-48e69.firebaseapp.com",
+    projectId: "react-blog-app-48e69",
+    storageBucket: "react-blog-app-48e69.appspot.com",
+    messagingSenderId: "320289826160",
+    appId: "1:320289826160:web:00f4f89a9a8a230a9d9bb5"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
